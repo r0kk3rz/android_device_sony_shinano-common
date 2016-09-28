75cf41d9d5b604fee9bfd4cca5c99b47d2cee271
Nokius <jupaja@gmx.net>
2016-09-28 19:59:32 +0000

    enable Camera

--- shinano.mk
+++ shinano.mk
@@ -188,6 +188,10 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
 
+# Camera
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    persist.camera.shutter.disable=1
+
 # BCM Wifi
 $(call inherit-product-if-exists, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
 
